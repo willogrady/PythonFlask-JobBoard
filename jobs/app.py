572b3786c074d1191263e90ff95a722d7727a02d
willogrady <william.ogrady@mastek.com>
2019-02-08 10:03:35 +0000

    app.route

--- jobs/app.py
+++ jobs/app.py
@@ -43,7 +43,7 @@ def job(job_id):
     job = execute_sql('SELECT job.id, job.title, job.description, job.salary, employer.id as employer_id, employer.name as employer_name FROM job JOIN employer ON employer.id = job.employer_id WHERE job.id = ?', [job_id], single=True)
     return render_template('job.html', job=job)
 
-app.route('/employer/<employer_id>')
+@app.route('/employer/<employer_id>')
 def employer(employer_id):
     employer = execute_sql('SELECT * FROM employer WHERE id=?', [employer_id], single=True)
     jobs = execute_sql('SELECT job.id, job.title, job.description, job.salary FROM job JOIN employer ON employer.id = job.employer_id WHERE employer.id = ?', [employer_id])
